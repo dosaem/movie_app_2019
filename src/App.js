f93fd060c177cd9a7cd35254e22fcfb2f3325aa2
dosaem90 <dosaem90@gmail.com>
2019-08-23 17:05:57 +0900

    Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 
-function Food({ fav }) {
-  return <h1>i like {fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>i like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
 
-const foodILike = [{}];
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "http://1stladykimchi.co.kr/web/product/medium/15_shop1_549701.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "http://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
