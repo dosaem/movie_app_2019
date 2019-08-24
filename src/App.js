ec15380ac39374c9c4a13f25b8bd792a333c37f0
dosaem90 <dosaem90@gmail.com>
2019-08-24 23:35:14 +0900

    map Recap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from "react";
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>i like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image: "http://1stladykimchi.co.kr/web/product/medium/15_shop1_549701.jpg"
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "http://image.chosun.com/sitedata/image/201902/24/2019022401106_0.jpg"
   }
 ];
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>i like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
