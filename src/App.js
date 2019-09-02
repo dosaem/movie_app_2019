f9b549f464fb64bc8a130448b7a3e94c1a5108ff
dosaem90 <dosaem90@gmail.com>
2019-09-02 23:44:19 +0900

    Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movie: []
   };
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+
   render() {
-    return (
-      <div>
-        <h1>the number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus </button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
